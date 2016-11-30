7787463eca70a2336872ea6d5ce8a3ffca35c827
cpragadeesh <cpragadeesh@gmail.com>
2016-11-30 20:56:50 +0530

    added users_union.py

---
+++ users_union.py
@@ -0,0 +1,23 @@
+def find_union(file1, file2):
+	
+	user_list = []
+
+	with open(file1) as f1, open(file2) as f2:
+		
+		for line in f1:
+			user = line.strip()
+			if user not in user_list:
+				user_list.append(user)
+
+		for line in f2:
+			user = line.strip()
+			if user not in user_list:
+				user_list.append(user)
+
+	return user_list
+if __name__ == "__main__":
+
+	users = find_union("users_list_1.txt", "users_list_2.txt")
+
+	print "Found: " + str(len(users)) + " unique users"
+	print users
